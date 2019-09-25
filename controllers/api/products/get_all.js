0cb810c39bf2ad6597934d8836b441d1d13d850f
Nicole Lee <nicoledasullee@gmail.com>
2019-09-25 03:47:04 +0000

    Setup default error handler and added error handling to existing endpoints

--- controllers/api/products/get_all.js
+++ controllers/api/products/get_all.js
@@ -2,32 +2,37 @@ const db = require('../../../db');
 const {imageUrl} = require('../../../helpers');
 
 module.exports = async (req, res, next) => {
-    const [results] = await db.query(`
+    try {
+        const [results] = await db.query(`
             SELECT p.pid AS id, p.caption, p.cost, p.name, i.file, i.altText, i.type, i.pid AS imageId
             FROM products AS p 
             JOIN images AS i 
             ON i.productId=p.id 
             WHERE i.type="thumbnail"
             `);
-    const products = results.map(p => {
-        const {id, caption, cost, name, imageId, altText, file, type} = p;
-        const product = {
-            id,
-            caption,
-            cost,
-            name,
-            thumbnail: {
-                id: imageId,
-                altText,
-                file,
-                type,
-                url: imageUrl(req, type, file)
-            }
-        };
-        return product;
-    });
-    res.send({
-        products: products
-    });
+        const products = results.map(p => {
+            const {id, caption, cost, name, imageId, altText, file, type} = p;
+            const product = {
+                id,
+                caption,
+                cost,
+                name,
+                thumbnail: {
+                    id: imageId,
+                    altText,
+                    file,
+                    type,
+                    url: imageUrl(req, type, file)
+                }
+            };
+            return product;
+        });
+        res.send({
+            products: products
+        });
+    }
+    catch(err) {
+        next(err);
+    }
 };
 
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const PORT = process.env.PORT || 9000;
-
+const StatusError = require('./helpers/status_error');
+const defaultErrorHandler = require('./middleware/default_error_handler');
 const app = express();
 
-
+global.StatusError = StatusError;
 
 const routes = require('./routes');
 app.use(routes);
+
+//error handler is always at the very end
+app.use(defaultErrorHandler);
 
 app.listen(PORT, () => {
     console.log('Server running @ localhost:', PORT);
